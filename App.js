a2534b1005385693dcd2b05399a5e435517133e0
Nicholas Radtke <Raddnc@gmail.com>
2021-08-27 14:56:59 -0500

    working on basic website for learning reactnative

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Header from './components/Header';
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text> React Native Mob dev</Text>
-      <StatusBar style="auto" />
+     <Header/>
+     <CarsList/>
+     <StatusBar style="auto"/>
     </View>
   );
 }
